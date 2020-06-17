27936e19d3c12ff6896c30df729fd63a3e203159
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-17 17:51:30 -0400

    Setup welcome-router for at the root of the API

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const welcomeRouter = require('./car-api/welcome/welcome-router');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api', welcomeRouter);
 
 module.exports = server;
